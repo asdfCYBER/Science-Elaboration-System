439cb90f1bbf0c38c40bc6c8da5cbf7699d35faa
asdfCYBER <asdfCYBER@outlook.com>
2019-10-29 14:21:41 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Science Elaboration System")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Science Elaboration System")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("54d73182-9350-4bd1-9d99-ee1eb1957ad4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Science_Elaboration_System.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using CommNet;
+
+namespace Science_Elaboration_System
+{
+    /// <summary>Module for containing data</summary>
+    [KSPModule("ModuleDataContainer")]
+    public class ModuleDataContainer : PartModule
+    {
+        [KSPField(isPersistant = true)]
+        public int containerSize;
+
+        public Dictionary<string, float> containedData = new Dictionary<string, float>();
+
+        public override void OnLoad(ConfigNode node)
+        {
+            Debug.Log("[SES] OnLoad called");
+            Debug.Log(node.ToString());
+
+            base.OnLoad(node);
+            containedData.Clear();
+
+            if (node.HasNode("DATACONTAINER"))
+            {
+                ConfigNode dataNode = node.GetNode("DATACONTAINER");
+                int numEntries = dataNode.CountValues / 2;
+
+                for (int i = 1; i <= numEntries; i++)
+                {
+                    string key = dataNode.GetValue("ID_" + i);
+                    float value = float.Parse(dataNode.GetValue("size_" + i));
+                    containedData.Add(key, value);
+                    Debug.Log("entry " + i + " was added to containedData");
+                }
+            }
+
+            Debug.Log(DebugDictionary());
+            Debug.Log("[SES] OnLoad finished");
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            Debug.Log("[SES] OnSave called");
+            base.OnSave(node);
+            ConfigNode dataNode = new ConfigNode("DATACONTAINER");
+            node.AddNode(dataNode);
+
+            Debug.Log(DebugDictionary());
+
+            int index = 1;
+            foreach (KeyValuePair<string, float> entry in containedData)
+            {
+                dataNode.AddValue("ID_" + index, entry.Key);
+                dataNode.AddValue("size_" + index, entry.Value);
+                index++;
+            }
+            
+            Debug.Log(node.ToString());
+            Debug.Log("[SES] OnSave finished");
+        }
+
+        public string DebugDictionary()
+        {
+            string output = "";
+            foreach (KeyValuePair<string, float> entry in containedData)
+            {
+                output += entry.Key + ":" + entry.Value.ToString() + "; \n";
+            }
+            if (output == "") { return "containedData is empty"; }
+            return output;
+        }
+    }
+
+    /// <summary>Module for catching science experiment results and storing it on the vessel as data</summary>
+    [KSPModule("ModuleScienceInterrupter")]
+    public class ModuleScienceInterrupter : PartModule
+    {
+        public void Start()
+        {
+            GameEvents.OnExperimentStored.Add(ExperimentListener); //replace this later with OnExperimentDeployed and a custom GUI instead as a replacement of the current science report GUI
+        }
+        public void OnDisable()
+        {
+            GameEvents.OnExperimentStored.Remove(ExperimentListener);
+        }
+
+        public void ExperimentListener(ScienceData scienceData)
+        {
+            string ID = scienceData.subjectID;
+            float size = scienceData.dataAmount;
+            Debug.Log("[SES] Experiment catched: {subjectID}");
+            StoreData(ID, size);
+            //find storage
+            //store
+
+            //ScienceSubject scienceSubject = ResearchAndDevelopment.GetSubjectByID(scienceData.subjectID);
+            //Debug.Log(ResearchAndDevelopment.GetScienceValue(scienceData.dataAmount, scienceSubject)); //This is the science given when returned
+        }
+
+        public void StoreData(string _ID, float _size)
+        {
+            ModuleDataContainer storageModule = this.part.FindModuleImplementing<ModuleDataContainer>();
+            float maxStorage = storageModule.containerSize;
+            float usedStorage = storageModule.containedData.Values.Sum();
+
+            if (storageModule.containedData.ContainsKey(_ID))
+            {
+                Debug.LogWarning("[SES] " + _ID + " is already in the storage module!");
+            }
+            else if (usedStorage + _size <= maxStorage)
+            {
+                storageModule.containedData.Add(_ID, _size);
+                Debug.Log("[SES] storage used: " + (usedStorage + _size) + " of " + maxStorage);
+            }
+            else
+            {
+                Debug.LogWarning("[SES] " + _ID + " does not fit in the storage module: " + usedStorage + "+" + _size + " > " + maxStorage + "!");
+            }
+        }
+    }
+
+    /// <summary>Module for transmitting data</summary>
+    [KSPModule("ModuleDataTransmitter")]
+    public class ModuleDataTransmitter : PartModule
+    {
+
+    }
+}
